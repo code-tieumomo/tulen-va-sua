7e39f3911a9014c78a4a9e1bd137d5d70b0d1197
code-tieumomo <code.tieumomo@gmail.com>
2024-09-10 01:50:22 +0700

    fix: background

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="pattern-bg min-w-screen min-h-screen flex items-center justify-center bg-gray-100">
+  <div class="min-w-screen min-h-screen flex items-center justify-center bg-gray-100 bg-[url('~/assets/images/pattern.png')]">
     <img src="~/assets/images/tulen.png" alt="tulen" class="fixed bottom-0 left-0 w-1/3">
     <img src="~/assets/images/milk.png" alt="milk" class="fixed bottom-0 right-0 w-1/3">
     <span class="text-3xl lg:text-7xl font-black">Tulen và Sữa</span>
